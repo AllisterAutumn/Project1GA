3ab77937709580d074aa24af7641e5de14f01a74
val rigodon <rvrigodon@gmail.com>
2016-12-15 09:22:58 -0500

    this commit will add my MVP to my repo

---
+++ src/gameboard.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+
+$('.left').animate({bottom: "-=2500px"}, 8000);
+console.log('test');
+$('.right').animate({bottom: "-=2500px"}, 8000);
+//$('#main').animate({bottom: "-=2500px"}, 7000);
+//console.log('test');
+
+
+})
+
--- src/player.js
+++ src/player.js
@@ -3,39 +3,44 @@ $(document).keydown(function(key) {
 switch(key.which) {
   case 37:
   //left
-$('#square').animate({ left: "-=10px" }, 5);
+$('#square').animate({ left: "-=30px" }, .1);
 //right
     break;
   case 39 :
-$('#square').animate({ left: "+=10px" }, 5);
+$('#square').animate({ left: "+=30px" }, .1);
     break;
 //top
+  /* case 40 :
+$('#square').animate({ bottom: "-=30px" }, .1);
+    break;
+
      case 38 :
-$('#square').animate({ top: "-=10px" }, 5);
-    break;
+$('#square').animate({ top: "-=30px" }, .1);
+    break;*/
     //bottom
-     case 40 :
-$('#square').animate({ bottom: "-=10px" }, 10);
-    break;
 
-    /* case 39, 40 :
-$('#square').animate({ bottom: "-=10px",  left: "+=10px" }, 'fast');
-    break;*/
+/*case 32 :
+$('#square').css({
+  height: '80px',
+  width: '80px'
+});
+break;*/
 
 
-   /* case 32 :
-$('#laser').animate({ bottom: "-=500px" }, 200);
-$('#laser').animate({ bottom: "+=500px" }, 200);
-    break;*/
+//     case 32 :
+// $('#laser').animate({ bottom: "-=500px" }, 200);
+// $('#laser').animate({ bottom: "+=500px" }, 200);
+//     break;
 }
 
-
-
-
-  /* Act on the event */
 });
 
-
+/*$(document).keyup(function() {
+$('#square').css({
+  height: '50px',
+  width: '50px'
+});
+});*/
 
 
 })
