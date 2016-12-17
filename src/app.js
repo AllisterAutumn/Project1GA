f754816b4dc38fe48ed8463a50d3874bada16510
val rigodon <rvrigodon@gmail.com>
2016-12-17 17:58:13 -0500

    thiss commit will add to the win/lose feature

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ $(document).ready(function() {
 
     $('div').mouseover(function() {
         console.log('yo');
+        document.getElementById("main").scrollTop += 1300;
         //Act on the event
     });
 
