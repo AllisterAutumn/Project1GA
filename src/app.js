2190f519cdd8711ecfa2a5eefd9b9d125bcca7c2
val rigodon <rvrigodon@gmail.com>
2016-12-17 23:46:42 -0500

    this commit will add some final touches and designs to my game

--- src/app.js
+++ src/app.js
@@ -2,13 +2,50 @@ $(document).ready(function() {
 
 
 
-    $('div').mouseover(function() {
-        console.log('yo');
-        document.getElementById("main").scrollTop += 1300;
-        //Act on the event
-        alert('Stay Strong!')
-    });
+    class Enemy {
+        constructor(id, color, animation) {
+            this.id = id;
+            this.color = color;
+            this.x = null;
+            this.y = null;
+            this.animation = animation;
+        }
+        setPosition(x, y) {
 
 
+            this.x = x;
 
+            this.y = y;
+            return x + " " + y
+        }
+
+
+    }
+
+    let one = new Enemy('one', 'green', 'clockwise')
+    console.log(one);
+
+
+    console.log(one.setPosition($('#one').offset().left, $('#one').offset().top));
+
+
+    let foot = new Enemy('foot', 'brown', 'stomp');
+    $(document).keydown(function() {
+        foot.setPosition($('#foot').offset().left, $('#foot').offset().top);
+        console.log(foot);
+    })
 })
+
+
+
+
+
+
+
+//console.log(one.setPosition($('#one').offset().left, $('#one').offset().top))
+//console.log($('#one').offset().left);
+//console.log($('#one').offset().t);
+
+/*let one = new Enemy('one', 'green', 'clockwise' );
+console.log(one.setPosition($('#one').offset().left, $('#one').offset().top));
+console.log(one.x,one.y);*/
--- src/gameboard.js
+++ src/gameboard.js
@@ -2,50 +2,13 @@ $(document).ready(function() {
 
 
 
-    class Enemy {
-        constructor(id, color, animation) {
-            this.id = id;
-            this.color = color;
-            this.x = null;
-            this.y = null;
-            this.animation = animation;
-        }
-        setPosition(x, y) {
+    $('div').mouseover(function() {
+        console.log('):');
+        document.getElementById("main").scrollTop += 1300;
+
+        alert("Stay Strong! You'll reach the top soon!")
+    });
 
 
-            this.x = x;
 
-            this.y = y;
-            return x + " " + y
-        }
-
-
-    }
-
-    let one = new Enemy('one', 'green', 'clockwise')
-    console.log(one);
-
-
-    console.log(one.setPosition($('#one').offset().left, $('#one').offset().top));
-
-
-    let foot = new Enemy('foot', 'brown', 'stomp');
-    $(document).keydown(function() {
-        foot.setPosition($('#foot').offset().left, $('#foot').offset().top);
-        console.log(foot);
-    })
 })
-
-
-
-
-
-
-
-//console.log(one.setPosition($('#one').offset().left, $('#one').offset().top))
-//console.log($('#one').offset().left);
-//console.log($('#one').offset().t);
-
-/*let one = new Enemy('one', 'green', 'clockwise' );
-console.log(one.setPosition($('#one').offset().left, $('#one').offset().top));
-console.log(one.x,one.y);*/
